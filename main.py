320423df69a8188faa2f18e75a452a141abe64a4
giacomocamposampiero <giacomo.camposampiero@hotmail.com>
2022-07-05 08:58:45 +0200

    Rebase completed.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from calendar import EPOCH
+from configparser import ExtendedInterpolation
 from utils.utils import get_input_matrix, generate_submission, submit_on_kaggle
 from utils.config import *
 import numpy as np
-from models.matrix_factorization import ALS
+from models.matrix_factorization import ALS, NMF
 from models.dimensionality_reduction import SVD
 from models.clustering import BCA
 
@@ -13,23 +14,32 @@ def main():
     # load data
     print("Loading data...")
     X, W = get_input_matrix()
-    # SVD
-    experiments_on_svd_rank(X, W)
-    # ALS
-    experiments_on_als_rank(X, W)
+
+    
 
 def experiments_on_svd_rank(X, W):
-    ranks = range(1, 15, 1)
-    for k in ranks:
-        model = SVD(k, N_USERS, N_MOVIES, k, verbose=0)
-        model.fit(X, None, W, 0.2)
+    i = 0
+    for k in range(2, 30):
+        print(f"Rank {k}")
+        model = SVD(i, N_USERS, N_MOVIES, k, verbose=0)
+        model.fit(X, None, W, 0.2, imputation='zeros')
         model.log_model_info()
+        i+=1
+
+def experiments_on_nmf_rank(X, W):
+    i = 0
+    for k in range(2, 30):
+        print(f"Rank {k}")
+        model = NMF(i, N_USERS, N_MOVIES, k)
+        model.fit(X, None, W, 0.2, imputation='mean', iter=1024)
+        model.log_model_info()
+        i+=1
 
 def experiments_on_als_rank(X, W):
-    ranks = range(1, 15, 1)
-    for k in ranks:
-        model = ALS(k, N_USERS, N_MOVIES, k, verbose=0)
-        model.fit(X, None, W, epochs=10, test_size=0.2, n_jobs=-1)
+    for k in [2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 15, 20, 25]:
+        print(f"Rank {k}...")
+        model = ALS(k, N_USERS, N_MOVIES, k, verbose=1)
+        model.fit(X, None, W, epochs=20, test_size=0.2, )
         model.log_model_info()
 
 if __name__ == '__main__':
